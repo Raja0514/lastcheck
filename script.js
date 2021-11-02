d815351f1a1bb00c470d68c765f37bcc1883dfa8
Raja Mathiyalagan <87691278+Raja0514@users.noreply.github.com>
2021-11-02 18:41:43 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const para=document.querySelector('.paragraph');
+
+
+ function onClick(){
+
+    fetch("http://www.boredapi.com/api/activity")
+
+    .then(response=>{
+
+        return response.json();
+        })
+        
+        .then(data=>{
+
+            console.log(data);
+        
+            document.querySelector('.paragraph').textContent=data.activity;
+        
+        })
+
+        .catch(error=>{
+            console.log('error');
+            console.error(error);
+
+            document.write(error);
+        })
+        
+        
+    
+
+    // let data= await response.json();
+
+    
+
+    // para.textContent=data.activity;
+}
+    
